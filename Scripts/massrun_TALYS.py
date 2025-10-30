8a57bbead5a67b7154810290326a6c842ffebee0
Antero Voutilainen <antero.vouti@outlook.com>
2025-10-30 14:36:10 +0200

    Prepare for TALYS

---
+++ Scripts/massrun_TALYS.py
@@ -0,0 +1,15 @@
+r'''
+Copyright (c) 2025 Antero Voutilainen
+Created: 30 10 2025
+
+Description: 
+'''
+import argparse as ap
+
+def main():
+    ap = ap.ArgumentParser(des)
+    return
+
+
+if __name__ == "__main__":
+    main()
